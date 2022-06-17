109871c6917205058f4d271637b012072d3e3316
caimingA <caiming106@sina.com>
2022-06-18 07:10:49 +0900

    Site updated: 2022-06-18 07:10:46

--- libs/codeBlock/codeCopy.js
+++ libs/codeBlock/codeCopy.js
@@ -1,13 +1,13 @@
 // 代码块一键复制
 $(function () {
-    var $copyIcon = $('<i class="fa fa-files-o code_copy" title="复制代码" aria-hidden="true"></i>')
+    var $copyIcon = $('<i class="fas fa-copy code_copy" title="复制代码" aria-hidden="true"></i>')
     var $notice = $('<div class="codecopy_notice"></div>')
     $('.code-area').prepend($copyIcon)
     $('.code-area').prepend($notice)
     // “复制成功”字出现
     function copy(text, ctx) {
+        // if (window.getSelection() && window.getSelection('copy')('copy')) {
         if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
-        // if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
             try {
                 document.execCommand('copy') // Security exception may be thrown by some browsers.
                 $(ctx).prev('.codecopy_notice')
@@ -44,7 +44,7 @@ $(function () {
         }
     }
     // 复制
-    $('.code-area .fa-files-o').on('click', function () {
+    $('.code-area .fa-files').on('click', function () {
         var selection = window.getSelection()
         var range = document.createRange()
         range.selectNodeContents($(this).siblings('pre').find('code')[0])
